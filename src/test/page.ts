6aba1aec02ad650bdd26998c134bbca42f4efbac
Tony Samperi <github@tonysamperi.it>
2022-06-01 10:55:56 +0200

    Improved test page

--- src/test/page.ts
+++ src/test/page.ts
@@ -1,4 +1,5 @@
 import {MediaObserverService, MediaChange} from "../index";
+import {debounceTime} from "rxjs/operators";
 
 class Page {
     private _bo: MediaObserverService = MediaObserverService.INSTANCE;
@@ -6,7 +7,9 @@ class Page {
 
     constructor() {
         console.info("PAGE CONSTRUCTOR");
-        this._bo.asObservable().subscribe(($event: MediaChange[]) => {
+        this._bo.asObservable()
+            .pipe(debounceTime(250))
+            .subscribe(($event: MediaChange[]) => {
             console.info("GOT CHANGES", $event);
             console.info("TEST", {
                 gtxs: this._bo.isActive("gtxs"),
@@ -16,6 +19,7 @@ class Page {
                 ltmd: this._bo.isActive("lt-md"),
                 ltlg: this._bo.isActive("lt-ltlg"),
             });
+            this._write($event[0].mqAlias);
         });
         this._write("Loading...");
     }
